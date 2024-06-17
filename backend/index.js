f94037811a751c5d3e93d0a5f352a214b86a3ed9
ebincodes5599 <ebincodes5599@gmail.com>
2024-06-17 18:24:57 +0100

    change in index.js

--- backend/index.js
+++ backend/index.js
@@ -1,38 +1,36 @@
+const dotenv = require('dotenv');
+dotenv.config();
 
-const dotenv=require('dotenv');
-
-const express =require('express');
+const express = require('express');
 const app = express();
+const path = require('path');
 const connectDB = require('./db/db');
 const rootRouter = require('./routes');
-const cors =require('cors')
-const bodyParser = require('body-parser');
-const jwt = require('jsonwebtoken');
+const cors = require('cors');
 
-
-dotenv.config();
+// Set Port
 const PORT = process.env.PORT || 5000;
 
-//middlewares
+// Middlewares
 app.use(express.json());
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(cors());
-app.use(bodyParser.json());
 
-//routes
-app.use('/api/v1/',rootRouter);
+// Routes
+app.use('/api/v1/', rootRouter);
 
-//connect to db
+// Connect to DB
 connectDB();
 
+// Serve Static Files
 app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
+// Catch-All Route for Client-Side Routing
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
 });
 
-
-//listening to Server
-app.listen(PORT,()=>{
-    console.log(`Server is running on port ${PORT}`);
-})
+// Start the Server
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
